fb6607dd85c34261c8f44f7572b9eca5f718323b
Tiffany Marroquin <tiffany.marroq@gmail.com>
2018-09-11 19:45:50 -0400

    added event page

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles',99);
 function child_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/dist/bundle-style.min.css', array( $parent_style ) );
 }
 if ( get_stylesheet() !== get_template() ) {
     add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
