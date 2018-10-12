d17a348a364984eed203df5aca61a5c222d4d861
Tiffany Marroquin <tiffany.marroq@gmail.com>
2018-10-11 20:30:55 -0400

    test

--- functions.php
+++ functions.php
@@ -3,11 +3,7 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles',99);
 function child_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-<<<<<<< HEAD
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/styles/style.css', array( $parent_style ) );
-=======
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/dist/bundle-style.min.css', array( $parent_style ) );
->>>>>>> 9bab91224bce3b282edf476a72c6ff6c34c214a8
 }
 if ( get_stylesheet() !== get_template() ) {
     add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
